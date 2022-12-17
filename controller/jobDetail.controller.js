4c4796c347a00b52e4c2c2eac708e68ae542e0d8
shilpashingnapure <shingnapureshilpa2@gmail.com>
2022-12-17 15:18:14 +0530

    pagination trying

--- controller/jobDetail.controller.js
+++ controller/jobDetail.controller.js
@@ -11,31 +11,22 @@ const jobPost = async (req , res)=>{
 }
 
 const getJobPost = async (req , res)=>{
-    let limit = +req.query.limit
-    let page = +req.query.page
+
     try{
-        if(!page){
-            page = 1
-            limit = 10
 
-        }
-        else{
-            limit = page * 10
-        }
 
-        // console.log(page , limit)
-        let JobPost = await JobDetail.find().limit(limit)
+        let JobPost = await JobDetail.find()
         if(req.query.filter != undefined){
             console.log(req.query)
-            JobPost = await JobDetail.find({role:req.query.filter}).limit(limit)
+            JobPost = await JobDetail.find({role:req.query.filter})
 
         }
         if(req.query.sort != undefined){
             if(req.query.sort == 'asec'){
-                JobPost = await JobDetail.find().sort({createdAt : 1}).limit(limit)
+                JobPost = await JobDetail.find().sort({createdAt : 1})
 
             }else if(req.query.sort == 'dec'){
-                JobPost = await JobDetail.find().sort({createdAt : -1}).limit(limit)
+                JobPost = await JobDetail.find().sort({createdAt : -1})
 
             }
 
@@ -43,7 +34,7 @@ const getJobPost = async (req , res)=>{
         if(req.query.q != undefined){
             JobPost = await JobDetail.find({language:req.query.q})
         }
-        return res.send(JobPost)
+        return res.send({JobPost , totalPage : JobPost.length})
     }catch(e){
         return res.status(500)
     }
